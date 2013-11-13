0d4c779de4e260288c98c319a78e290be46df2ba
xuanzhaopeng <xuanzhaopeng@gmail.com>
2013-11-13 12:11:25 +0100

    Update steps.rb
    sss

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'watir-webdriver'
 
 Given /^I open (.*) and the (.*)$/ do |browser,url|
-  browser = Watir::Browser.new
-  browser.goto url
+  $browser = Watir::Browser.new
+  $browser.goto url
 end
 
 When /^the browser statu is open$/ do 
@@ -11,5 +11,5 @@ When /^the browser statu is open$/ do
 end
 
 Then /^I type (.*) and click button$/ do |value|
-	browser.close
+	$browser.close
 end
